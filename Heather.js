49d8cd5541c8d27fc5b2c848a1afed9222c22af3
irenilona <110005452+irenilona@users.noreply.github.com>
2022-07-25 20:08:40 -0700

    Update Heather.js

--- Heather.js
+++ Heather.js
@@ -1,33 +1,21 @@
+let i = 1,
+letterInterval = 100,
+pauseBetweenWords = 300; //ms
+const sentence = `Hello lovely world. I think it's gonna be a really beautiful day!`;
+const words = sentence.split(' ');
+let index = 0;
+const runTyping = (ms) => setTimeout(() => showText(), ms);
 
-let interval = [];
-
-// -------------- USER - INTERFACE --------------------
-let txtInput = [];
-    txtInput[0] = "Website Designer \n SEO Analyst"; 
-    txtInput[1] = "What the hell!!! Another text showing as Type-Writer-Effect???"; 
-    txtInput[2] = "Wow, it's amazing!!!"; 
-
-let DELAY = 500;
-let timeInterval = 50;
-let SYMBOL = "|"
-// -------------- USER - INTERFACE --------------------
-
-
-$w.onReady(async function() {
-    for (let i = 0; i < txtInput.length; i++) {const element = txtInput[i]; xxx(i);}
-
-    function xxx(value){
-        setTimeout(()=>{
-            let typeIdx = 0, displayStr = "", endingString = SYMBOL;
-            interval[value] = setInterval(() => {
-                displayStr = displayStr + txtInput[value][typeIdx]
-                typeIdx++
-
-                $w("#twInput"+value).text = displayStr + endingString
-
-                typeIdx === txtInput[value].length && clearInterval(interval[value]);
-            }, timeInterval)
-        },DELAY);
-        DELAY=DELAY+500;
-    }    
-});
+function showText() {
+	const text = words[index];
+	$w('#text1').text = text.slice(0, i);
+	i = (i + 1) % (text.length + 1);
+	if (i > 0) {
+		runTyping(letterInterval);
+	} else {
+		index++;
+		index %= (words.length);
+		runTyping(pauseBetweenWords);
+	}
+}
+$w.onReady(() => runTyping(letterInterval));
